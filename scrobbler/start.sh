d7532f1dffffc66ada2001f1b175d705c19cb4d4
mdrxy <61371264+mdrxy@users.noreply.github.com>
2023-12-27 01:45:33 -0600

    switch to printf

--- scrobbler/start.sh
+++ scrobbler/start.sh
@@ -2,14 +2,14 @@
 
 # If setup hasn't been completed
 if [ ! -e /env/setup_done ]; then
-    echo -e "\e[34mstart.sh: Running initial setup...\e[0m"
+    printf "\e[34mstart.sh: Running initial setup...\e[0m"
     python scrobbler.py --setup
     
     if [ -e /env/setup_done ]; then
-        echo -e "\e[34mstart.sh: Setup completed successfully. Reboot the container to begin running scrobbler.\e[0m"
+        printf "\e[34mstart.sh: Setup completed successfully. Reboot the container to begin running scrobbler.\e[0m"
         exit 0
     else
-        echo -e "\e[91mstart.sh: Setup failed. Please check the logs for details.\e[0m"
+        printf "\e[91mstart.sh: Setup failed. Please check the logs for details.\e[0m"
         exit 1
     fi
 fi
